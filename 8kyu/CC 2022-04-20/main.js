08125df96e11f520fece799c2ac3d3b1298402b2
dtothehill <danielhillegas@gmail.com>
2022-04-20 12:55:53 -0400

    added 17th solution

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -263,3 +263,47 @@ let orderOperations = ()  => (2 + 2) * (2 + 2) * 2
 // Find the mean (average) of a list of numbers in an array.
 
 var findAverage = nums => nums.reduce((a,c) => a + c) / nums.length
+
+
+//====== Object Syntax debug ======//
+
+// While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.
+
+
+
+
+// original//
+
+// var rooms = {
+//   first: {
+//     description: 'This is the first room'
+//     items: {
+//       chair: 'The old chair looks comfortable',
+//       lamp: 'This lamp looks ancient'
+//   },
+//   second: {
+//     description: 'This is the second room'
+//     items: {
+//       couch: 'This couch looks like it would hurt your back,
+//       table: 'On the table there is an unopened bottle of water'
+//     }
+//   }
+// }
+
+//== solution==//
+let room = {
+  first: {
+    description: 'This is the first room',
+    items: {
+      chair: 'The old chair looks comfortable',
+      lamp: 'This lamp looks ancient'
+      },
+  },
+  second: {
+    description: 'This is the second room',
+    items: {
+      couch: 'This couch looks like it would hurt your back',
+      table: 'On the table there is an unopened bottle of water'
+    }
+  }
+}
