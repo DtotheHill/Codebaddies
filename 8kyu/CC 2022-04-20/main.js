bb83fb7372449f75c10edcd4dcf0a853f36e16dc
dtothehill <danielhillegas@gmail.com>
2022-04-20 11:57:43 -0400

    added 9th challenge

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -138,4 +138,30 @@ function sayHello(name) {
 let sayHello = name => `Hello, ${name}`
 
 
+//====== 8kyu Grasshopper - Grade Book ======//
 
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+
+function getGrade (s1, s2, s3) {
+  const avg = (s1 + s2 + s3) / 3
+
+ if (avg >= 90) {
+    return 'A';
+  } else if (avg >= 80) {
+    return 'B';
+  } else if (avg >= 70) {
+    return 'C';
+  } else if (avg >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
