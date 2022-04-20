6a9fa217e98ceebfc7b5354426bda99fc08e4e69
dtothehill <danielhillegas@gmail.com>
2022-04-20 12:41:30 -0400

    added 15th solution

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -246,3 +246,13 @@ const rooms = {
 
 let combineNames = (n1,n2) => `${n1} ${n2}`
 
+//====== Order of operations ======//
+
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+//original 
+// function orderOperations () {
+//   return 2 + 2 * 2 + 2 * 2
+// }
+
+let orderOperations = ()  => (2 + 2) * (2 + 2) * 2
