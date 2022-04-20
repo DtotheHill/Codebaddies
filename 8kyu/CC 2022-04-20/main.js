73146c948b69865b6d4be6ede993345eb8a140ec
dtothehill <danielhillegas@gmail.com>
2022-04-20 11:02:29 -0400

    added 4th challenge

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -66,3 +66,13 @@ function getRealFloor(n) {
   }
 
 let getRealFloor = n => (n >= 13 ) ? n-2 : (n > 0) ? n-1 : n
+
+
+// ====== 8 kyu Super Duper Easy ======//
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+
+function problem(x){
+  return typeof x == 'string' ? 'Error' : (x * 50 + 6);
+}
