9f66bd64eebdd80986780952769e7169d442fd08
dtothehill <danielhillegas@gmail.com>
2022-04-20 11:12:49 -0400

    added 5th coding challenge

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -76,3 +76,19 @@ let getRealFloor = n => (n >= 13 ) ? n-2 : (n > 0) ? n-1 : n
 function problem(x){
   return typeof x == 'string' ? 'Error' : (x * 50 + 6);
 }
+
+// ====== 8 kyu Training JS #7: if..else and ternary operator ======//
+
+
+// Task:
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+// number	price (cents)
+// n < 5	100
+// n >= 5 and n < 10	95
+// n >= 10	90
+// You can use if..else or ternary operator to complete it.
+
+let saleHotdogs = n => (n < 5) ? n * 100 : (n >=5 && n < 10) ? n * 95 : n * 90
+
+
