4b6afa75e4371484f8b67279452b915dc6c43bae
dtothehill <danielhillegas@gmail.com>
2022-04-20 13:06:39 -0400

    added 17th solution

--- 8kyu/CC 2022-04-20/main.js
+++ 8kyu/CC 2022-04-20/main.js
@@ -274,7 +274,7 @@ var findAverage = nums => nums.reduce((a,c) => a + c) / nums.length
 
 // original//
 
-// var rooms = {
+// var room = {
 //   first: {
 //     description: 'This is the first room'
 //     items: {
@@ -306,4 +306,23 @@ let room = {
       table: 'On the table there is an unopened bottle of water'
     }
   }
+}
+
+//====== 8kyu Terminal Game #1 ======//
+
+// In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+
+// attribute	value
+// name	user argument or 'Hero'
+// position	'00'
+// health	100
+// damage	5
+// experience	0
+
+function Hero (name) {
+  this.name = name || 'Hero';
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
 }
