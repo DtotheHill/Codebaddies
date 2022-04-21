10844dde6119a258ee0e2c873236775608a7f160
dtothehill <danielhillegas@gmail.com>
2022-04-21 16:24:30 -0400

    added 15th solution

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -227,3 +227,40 @@ function shortcut (string) {
 function take(arr, n) {
   return arr.slice(0,n)
 }
+
+
+//====== 8kyu Is this my tail ======//
+
+// Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+// If the tail is right return true, else return false.
+
+// The arguments will always be strings, and normal letters.
+
+// For Haskell, body has the type of String and tail has the type of Char. For Go, body has type string and tail has type rune.
+
+
+// original
+// function correctTail(bod, tail ;
+  
+//   sub = body.substr(bodylength-(tail.length)
+  
+//   if sub = tail) ;
+//     return true
+//   }
+//   else 
+//     return false
+
+
+
+    //solution
+function correctTail(body, tail){ 
+  let sub = body[body.length -1]
+  if (sub == tail){
+    return true
+  }
+  else{ 
+    return false
+  }
+}
+
