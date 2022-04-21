02854c049f198d34537eddca8978edc1f67222ea
dtothehill <danielhillegas@gmail.com>
2022-04-21 11:01:35 -0400

    added 6th solution

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -84,3 +84,29 @@ function hello(name) {
   return `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
   }
 }
+
+
+// ====== 8kyu Basic Mathematical Operations ======//
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+      case "+":
+        return value1 + value2;
+        break;
+      case "-":
+        return value1 - value2;
+        break;
+      case "*":
+        return value1 * value2;
+        break;
+      case "/":
+        return value1 / value2;
+        break;
+  }
+}
+
