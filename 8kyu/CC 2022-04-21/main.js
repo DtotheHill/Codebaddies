31ab14a43cab9e3f53189c1c3e90bde61f2d55e7
dtothehill <danielhillegas@gmail.com>
2022-04-21 10:58:59 -0400

    Added 5th solution

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -69,3 +69,18 @@ function replace(s){
   
 }
 
+
+
+// ====== 8kyu Hello name or world ======//
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+function hello(name) {
+  if (!name){
+    return 'Hello, World!'
+  } else{
+  return `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`
+  }
+}
