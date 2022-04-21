0f36f54dfa8be1a8d05a2d5846c7abb2d1c5b0b4
dtothehill <danielhillegas@gmail.com>
2022-04-21 15:39:35 -0400

    11th solution added

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -168,3 +168,21 @@ function hexToDec(hexString){
   return parseInt(hexString, 16);
 }
 
+// ===== 8kyu No zeros for heros ======//
+
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+
+function noBoringZeros(n) {
+  if (n == 0){
+    return 0
+  }else
+  while (n % 10 === 0){
+    n /= 10
+  }
+  return n
+}
