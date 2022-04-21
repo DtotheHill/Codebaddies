8e60d656d2883eb1fa6ac033565fefde4b1cd6ca
dtothehill <danielhillegas@gmail.com>
2022-04-21 17:46:48 -0400

    added 20th solution

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -323,3 +323,26 @@ function dutyFree(normPrice, discount, hol){
   let dfPrice = (normPrice*(discount/100))
   return Math.floor(hol/dfPrice)
 }
+
+//====== 8kyu Replace all donts ======//
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+
+// original
+// var replaceDots = function(str) {
+//   return str.replace(/./, '-');
+// }
+
+// solution
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
+}
