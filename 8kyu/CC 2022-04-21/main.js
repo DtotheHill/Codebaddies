401399b51fb04bc8ba6afe7e3cbceeb770f46767
dtothehill <danielhillegas@gmail.com>
2022-04-21 16:06:29 -0400

    added 13th solution

--- 8kyu/CC 2022-04-21/main.js
+++ 8kyu/CC 2022-04-21/main.js
@@ -209,3 +209,12 @@ function weatherInfo (temp) {
   }
 }
 
+
+//======= 8kyu Vowel remover ======//
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+function shortcut (string) {
+  let vowels = ['a','e','i','o','u']
+  return string.split('').filter(item => !vowels.includes(item)).join('')
+}
