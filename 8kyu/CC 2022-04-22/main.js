1afb953faa3d78453b56abe2695473289dd9966e
dtothehill <danielhillegas@gmail.com>
2022-04-22 11:24:59 -0400

    added 5th solution

--- 8kyu/CC 2022-04-22/main.js
+++ 8kyu/CC 2022-04-22/main.js
@@ -100,3 +100,18 @@ function sakuraFall(v) {
 function parseF(s) {
     return parseFloat(s) ? parseFloat(s) : (s==0 && s!==false) ? 0 : null
 }
+
+// ======= 8kyu Coefficients of the Quadratic Equation ======//
+
+// In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+// Equation will be the form of ax^2 + bx + c = 0
+
+// Return type is a Vector (tuple in Rust, Array in Ruby) containing coefficients of the equations in the order (a, b, c).
+
+// Since there are infinitely many solutions to this problem, we fix a = 1.
+
+function quadratic(x1, x2){
+    return [1, -x1-x2, x1*x2 ]
+}
+
