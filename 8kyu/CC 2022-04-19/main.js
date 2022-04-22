08ec3d29f328ff56229f1bd0d54506fd7fa9bf60
dtothehill <danielhillegas@gmail.com>
2022-04-22 13:18:28 -0400

    added coding challenge for 4-19-22

---
+++ 8kyu/CC 2022-04-19/main.js
@@ -0,0 +1,30 @@
+// ====== 8kyu Bug Squashing ======//
+
+// You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+
+// // Original
+// var health = 100
+// var position = 0
+// var coins = 0
+
+// function main () {
+//   getCoins()
+//   move()
+//   printStatus()
+//   combat()
+//   rolDice()
+//   attack()
+// }
+
+
+// Solution
+
+var health = 100
+var position = 0
+var coins = 0
+
+const main = () =>
+  [rollDice, move, combat, getCoins, buyHealth, printStatus].forEach(action =>
+    action()
+  )
+  
