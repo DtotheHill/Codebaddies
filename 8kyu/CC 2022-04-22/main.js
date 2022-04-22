f100ae5d6b2dc79f8936f8963652dea1f19bacfb
dtothehill <danielhillegas@gmail.com>
2022-04-22 10:24:02 -0400

    added 3rd solution

--- 8kyu/CC 2022-04-22/main.js
+++ 8kyu/CC 2022-04-22/main.js
@@ -35,3 +35,64 @@ function derive(coefficient,exponent) {
 function derive(coefficient,exponent) {
     return `${coefficient*exponent}x^${exponent-1}`
 }
+
+
+// ====== 8kyu Holiday VI - Shark Pontoon ======//
+
+// Your friend invites you out to a cool floating pontoon around 1km off the beach. Among other things, the pontoon has a huge slide that drops you out right into the ocean, a small way from a set of stairs used to climb out.
+
+// As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+
+// You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+
+// You are given 5 variables;
+
+// sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+// sharkSpeed = how fast it can move in metres/second.
+
+// pontoonDistance = how far you need to swim to safety in metres.
+
+// youSpeed = how fast you can swim in metres/second.
+
+// dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+// The pontoon, you, and the shark are all aligned in one dimension.
+
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    sharkSpeed = (dolphin) && sharkSpeed/2
+    let attackDistance = sharkDistance - pontoonDistance
+    let closingSpeed = (sharkSpeed > youSpeed) ? sharkSpeed - youSpeed :
+    let safetyTime = pontoonDistance/youSpeed
+    let sharkTime = attackDistance/closingSpeed
+    return safetyTime <+ sharkTime ? 'Alive!' : 'Shark Bait!'
+  }
+
+
+
+
+// ====== 8kyu The falling speed of petals ======//
+
+// When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+
+// Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+// Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+
+// Notes:
+
+// The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+// Pay attention to the data types.
+// If the initial velocity is non-positive, the return value should be 0
+
+
+function sakuraFall(v) {
+    if(v <= 0){
+    return 0;
+     }else{
+    return (80*5)/v;
+   }
+   }
