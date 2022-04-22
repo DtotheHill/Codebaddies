e7af0f476ba068d2749718bff50a5d7e1fdba23c
dtothehill <danielhillegas@gmail.com>
2022-04-22 10:38:18 -0400

    added 4th solution

--- 8kyu/CC 2022-04-22/main.js
+++ 8kyu/CC 2022-04-22/main.js
@@ -95,4 +95,12 @@ function sakuraFall(v) {
      }else{
     return (80*5)/v;
    }
-   }
+}
+
+//====== 8kyu Parse Float ======//
+
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+function parseF(s) {
+    return parseFloat(s) ? parseFloat(s) : (s==0 && s!==false) ? 0 : null
+}
