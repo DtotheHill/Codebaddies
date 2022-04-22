9b60770059808f7410e0dc4ba7296eeccdc59311
dtothehill <danielhillegas@gmail.com>
2022-04-22 15:43:42 -0400

    added 11th solution

--- 8kyu/CC 2022-04-22/main.js
+++ 8kyu/CC 2022-04-22/main.js
@@ -187,3 +187,29 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
 
 let isValid = a => !(a.includes(1) && a.includes(2)) && !(a.includes(3) && a.includes(4)) && a.includes(5) === a.includes(6) && (a.includes(7) || a.includes(8));
 
+// ====== 8kyu Define a card suit ======//
+
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    if(card.includes("♣")){
+      return "clubs";
+    }else if(card.includes("♦")){
+      return "diamonds";
+    }else if(card.includes("♥")){
+      return "hearts"
+    }else {
+      return "spades"
+    }
+}
