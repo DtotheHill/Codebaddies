4dc7a323600bdbc8a2f40502b2384ee532a2d715
dtothehill <danielhillegas@gmail.com>
2022-04-23 10:48:40 -0400

    added 3rd solution

--- 8kyu/CC 2022-04-23/main.js
+++ 8kyu/CC 2022-04-23/main.js
@@ -19,3 +19,13 @@ function aspectRatio(x,y){
 function binToDec(bin){
   return parseInt(bin, 2)
 }
+
+
+
+// ====== 8kyu Remove First and Last Character ======//
+
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+  return str.slice(1,-1)
+}
