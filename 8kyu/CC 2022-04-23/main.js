8536dfe8d390c94ae9f64b70d387641a89933858
dtothehill <danielhillegas@gmail.com>
2022-04-23 12:06:18 -0400

    added 14th solution

--- 8kyu/CC 2022-04-23/main.js
+++ 8kyu/CC 2022-04-23/main.js
@@ -164,6 +164,17 @@ function find_average(array) {
     return 0;
   }
    return array.reduce((a, b) => a +b) / array.length;
- }
+}
 
- 
+
+// ====== Simple Multiplication ======//
+
+//  This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+ function simpleMultiplication(number) {
+  if (number % 2 === 0){
+    return number * 8
+  }else {
+    return number * 9
+  }
+}
