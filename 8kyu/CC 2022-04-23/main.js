97d030e53e7f8dc27bfee9d3f154363e959c6261
dtothehill <danielhillegas@gmail.com>
2022-04-23 12:03:26 -0400

    added 13th solution

--- 8kyu/CC 2022-04-23/main.js
+++ 8kyu/CC 2022-04-23/main.js
@@ -155,3 +155,15 @@ function bmi(weight, height) {
   }
 }
 
+// ====== 8kyu Calculate average ======//
+
+// Write a function which calculates the average of the numbers in a given list.
+
+function find_average(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+   return array.reduce((a, b) => a +b) / array.length;
+ }
+
+ 
