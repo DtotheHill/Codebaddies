fb082ad845cc68dcdf3c34f24b47efcee2dff4a2
dtothehill <danielhillegas@gmail.com>
2022-04-23 20:08:16 -0400

    added 28th solution

--- 8kyu/CC 2022-04-23/main.js
+++ 8kyu/CC 2022-04-23/main.js
@@ -374,3 +374,48 @@ function mouthSize(animal) {
 function remainder(n, m){
   return n > m ? n % m : m % n 
 }
+
+
+// ===== 8lyui Alan Partridge II - Apple Turnover ======//
+
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+function apple(x){
+  if (x**2 > 1000){
+    return 'It\'s hotter than the sun!!'
+  }else {
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
+}
+
+
+// ====== 8kyu How old will I be in 2099 ? ======//
+
+// Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+
+// Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+
+// Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+
+// "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+// Good Luck!
+
+function  calculateAge(birthDate, otherDate) {
+
+  var age = otherDate - birthDate;
+  
+    if(age === 1) {
+    return 'You are ' + age + ' year old.';
+      } else if(age > 1) {
+        return 'You are ' + age + ' years old.';
+      } else if (age < -1) {
+        return 'You will be born in ' +  Math.abs(age) + ' years.';
+      } else if (age === -1) {
+        return 'You will be born in ' + Math.abs(age) + ' year.';
+      } else {
+        return 'You were born this very year!';
+      }
+  }
+  
+
