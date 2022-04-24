d1e2dd191c78526678792774c88ed11e59d3ea29
dtothehill <danielhillegas@gmail.com>
2022-04-24 10:40:00 -0400

    added 2nd solution

--- 8kyu/CC 2022-04-24/main.js
+++ 8kyu/CC 2022-04-24/main.js
@@ -2,3 +2,14 @@
 
 // There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
 Object.freeze(MrFreeze)
+
+
+// ====== 8kyu Formatting decimal places #0 ======//
+
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+
+
+function twoDecimalPlaces(n) {
+  return (+(n.toFixed(2)))
+}
