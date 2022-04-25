59b6d520779f01f80493467ee2d825d43054d14c
dtothehill <danielhillegas@gmail.com>
2022-04-25 14:52:25 -0400

    added 17th solution

--- 8kyu/CC 2022-04-25/main.js
+++ 8kyu/CC 2022-04-25/main.js
@@ -243,3 +243,17 @@ function rentalCarCost(d) {
     return total
   }
 }
+
+
+// ====== Area or perimeter ======//
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function(l , w) {
+  if (l === w){
+    return l * w
+  }else {
+    return l * 2 + w * 2
+  }
+};
