bf222df68755cf6bd28e92bbff7a268d02f00702
dtothehill <danielhillegas@gmail.com>
2022-04-25 14:56:37 -0400

    added 18th solution

--- 8kyu/CC 2022-04-25/main.js
+++ 8kyu/CC 2022-04-25/main.js
@@ -257,3 +257,10 @@ const areaOrPerimeter = function(l , w) {
     return l * 2 + w * 2
   }
 };
+
+
+// ====== 8kyu Can we Divide it? =====//
+
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+const isDivideBy = (number, a, b) => number % a === 0 && number % b ===0
