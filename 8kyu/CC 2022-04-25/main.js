6ac1adf8a98c2e5146cae24051510803faf6989f
dtothehill <danielhillegas@gmail.com>
2022-04-25 14:36:31 -0400

    added 14th solution

--- 8kyu/CC 2022-04-25/main.js
+++ 8kyu/CC 2022-04-25/main.js
@@ -197,3 +197,11 @@ function cookie(x){
 function getNumberFromString(s) {
   return Number(s.match(/\d/g).join(''));
 }
+
+// ====== 8kyu Is n divisible by x and y? ======//
+
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
