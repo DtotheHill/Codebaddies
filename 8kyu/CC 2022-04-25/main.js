f77191c6dc7b3c2eff45537c1f9bacb2d0e97626
dtothehill <danielhillegas@gmail.com>
2022-04-25 13:47:25 -0400

    added 7th solution

--- 8kyu/CC 2022-04-25/main.js
+++ 8kyu/CC 2022-04-25/main.js
@@ -108,10 +108,17 @@ function grader(score) {
 //     results;
 // }
 
-
-
 // Solution
 function mystery() {
   var results = {sanity: 'Hello'};
   return results;
+}
+
+
+// 8kyu Price of Mangoes ======// 
+
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+function mango(quantity, price){
+  return price * (quantity - Math.floor(quantity / 3));
 }
