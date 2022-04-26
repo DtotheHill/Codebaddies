cb56612a2b2cffb38c45e79a405ab46525caf9f6
dtothehill <danielhillegas@gmail.com>
2022-04-26 16:26:34 -0400

    added 12th solution

--- 8kyu/CC 2022-04-26/main.js
+++ 8kyu/CC 2022-04-26/main.js
@@ -165,3 +165,13 @@ function hoopCount (n) {
     return 'Keep at it until you get it'
   }
 }
+
+
+// ====== 8kyu Sum the Strings ======//
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+  let convert = Number(a) + Number(b)
+  return convert.toString()
+}
