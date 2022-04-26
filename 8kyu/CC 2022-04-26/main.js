4c7600edc7ac21d2e8a04d984806d8175d34c256
dtothehill <danielhillegas@gmail.com>
2022-04-26 16:30:37 -0400

    added 13th solution

--- 8kyu/CC 2022-04-26/main.js
+++ 8kyu/CC 2022-04-26/main.js
@@ -175,3 +175,18 @@ function sumStr(a,b) {
   let convert = Number(a) + Number(b)
   return convert.toString()
 }
+
+
+// ====== 8kyu Enumerable Magic #3 - Does My List Include This? ======//
+
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+function include(arr, item){
+  if (arr.includes(item)){
+    return true
+  }else {
+    return false
+  }
+}
