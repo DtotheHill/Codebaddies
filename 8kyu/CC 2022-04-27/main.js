d79f597ddf16787a19de2720d6f3cc329c10d7f8
dtothehill <danielhillegas@gmail.com>
2022-04-27 15:20:19 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-27/main.js
+++ 8kyu/CC 2022-04-27/main.js
@@ -259,3 +259,26 @@ function findDifference([a,b,c], [d,e,f]) {
 function twoSort(s) {
   return s.sort()[0].split('').join('***')
 }
+
+
+// ====== 8kyu Stringy Strings ======//
+
+// write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+// the string should start with a 1.
+
+// a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// with size 12 should return : '101010101010'.
+
+// The size will always be positive and will only use whole numbers.
+
+function stringy(size) {
+  let str = ''
+  for (let i=1; i<=size; i++){
+    str+= i%2
+  }
+  return str
+}
