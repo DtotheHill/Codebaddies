fc47879ee5f393b212e4da9721465fe2c1c399c7
dtothehill <danielhillegas@gmail.com>
2022-04-27 14:27:12 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-27/main.js
+++ 8kyu/CC 2022-04-27/main.js
@@ -75,3 +75,13 @@ function getAge(inputString){
 function feast(beast, dish) {
   return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
 }
+
+
+
+//======8kyu Short long short ======//
+
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
+}
