be82ac9ee776a24218c89a2f6a18375024fd4625
dtothehill <danielhillegas@gmail.com>
2022-04-27 13:27:18 -0400

    added 7kyu 1st solution

--- 8kyu/CC 2022-04-27/main.js
+++ 8kyu/CC 2022-04-27/main.js
@@ -1,39 +1,8 @@
-// ====== 8kyu Vowel Count ======//
+// ====== 7kyu Highest and lowest ======//
 
-// Return the number (count) of vowels in the given string.
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
-// We will consider a, e, i, o, u as vowels for this Kata (but not y).
-
-// The input string will only consist of lower case letters and/or spaces.
-
-function getCount(str) {
-  let vowelsCount = 0;
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  for(let char of str) {
-    if(vowels.includes(char)) {
-      vowelsCount++;
-    }
-  }
-  return vowelsCount;
+function highAndLow(numbers){
+  let nums = numbers.split(' ')
+  return `${Math.max(...nums)} ${Math.min(...nums)}`
 }
-
-
-// ======8kyu Disemvowel Trolls ======//
-
-// Trolls are attacking your comment section!
-
-// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-
-// Your task is to write a function that takes a string and return a new string with all vowels removed.
-
-// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-
-// Note: for this kata y isn't considered a vowel.
-
-function disemvowel(str) {
-  let vowels = ['a','e','i','o','u']
-  return str.split('').filter((el)=> vowels.indexOf(el.toLowerCase())==-1).join('')
-}
-
-
-
