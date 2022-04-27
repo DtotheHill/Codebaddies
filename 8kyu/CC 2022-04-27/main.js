e967092e2ee666da7f4c08849c8bef87550fcb71
dtothehill <danielhillegas@gmail.com>
2022-04-27 14:41:51 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-27/main.js
+++ 8kyu/CC 2022-04-27/main.js
@@ -94,3 +94,15 @@ function solution(a, b) {
 function expressionMatter(a, b, c) {
   return Math.max( a+b+c, (a+b)*c, a*(b+c), a*b*c ) ;
 }
+
+// ====== 8kyu Switch it up ======//
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+function switchItUp(number){
+  var words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[number];
