2cd5aecfafc2fb63be61428d8579a52e32f00a21
dtothehill <danielhillegas@gmail.com>
2022-04-28 12:39:27 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-28/main.js
+++ 8kyu/CC 2022-04-28/main.js
@@ -41,3 +41,13 @@ function stringClean(s){
 function nameShuffler(str){
   return str.split(' ').reverse().join(' ')
 }
+
+
+
+// ====== 8kyu Semi-Optional ======//
+
+// We have implemented a function wrap(value) that takes a value of arbitrary type and wraps it in a new JavaScript Object or Python Dict setting the 'value' key on the new Object or Dict to the passed-in value.
+
+function wrap(value) {
+  return({value})
+}
