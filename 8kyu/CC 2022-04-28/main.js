ec918db70f9ec578ae8ccb97e18fafa7f12560aa
dtothehill <danielhillegas@gmail.com>
2022-04-28 14:38:20 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-28/main.js
+++ 8kyu/CC 2022-04-28/main.js
@@ -257,3 +257,15 @@ function arrayMadness(a,b) {
  function differenceInAges(ages){
   return [Math.min(...ages),Math.max(...ages),Math.max(...ages)-Math.min(...ages)]
 }
+
+// ====== 8kyu Add length =======//
+
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+// addLength('apple ban') => ["apple 5", "ban 3"]
+// addLength('you will win') => ["you 3", "will 4", "win 3"]
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+function addLength(str) {
+  return str.split(' ').map(word => `${word} ${word.length}`);
+}
