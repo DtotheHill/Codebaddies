32765da2b5976eccf5f7cb6f434a7fe98c8023d1
dtothehill <danielhillegas@gmail.com>
2022-04-28 13:50:50 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-28/main.js
+++ 8kyu/CC 2022-04-28/main.js
@@ -168,3 +168,31 @@ function plural(n) {
     return true
   }
 }
+
+// ====== 8kyu Suraface Area and Volume of a box ======//
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(l,w,d){
+  let area = 2*l*w + 2*l*d + 2*d*w
+  let volume = l * w * d
+  return[area, volume]
+}
+
+
+// ====== 8kyu Sleigh Authentication ======//
+
+// Christmas is coming and many people dreamed of having a ride with Santa's sleigh. But, of course, only Santa himself is allowed to use this wonderful transportation. And in order to make sure, that only he can board the sleigh, there's an authentication mechanism.
+
+// Your task is to implement the authenticate() method of the sleigh, which takes the name of the person, who wants to board the sleigh and a secret password. If, and only if, the name equals "Santa Claus" and the password is "Ho Ho Ho!" (yes, even Santa has a secret password with uppercase and lowercase letters and special characters :D), the return value must be true. Otherwise it should return false.
+
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if (name === 'Santa Claus' && password === 'Ho Ho Ho!'){
+    return true
+  }else {
+    return false
+  }
+};
