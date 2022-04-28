8e813dae81ef5789131eb08fa656ded7571f2d6f
dtothehill <danielhillegas@gmail.com>
2022-04-28 13:41:02 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-28/main.js
+++ 8kyu/CC 2022-04-28/main.js
@@ -137,3 +137,34 @@ function paperwork(n, m) {
     return 0
   }
 }
+
+// ====== DNA to RNA Conversion ======//
+
+function DNAtoRNA(dna) {
+  return dna.split("T").join("U");
+}
+
+// ====== Swap Values ======//
+
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+function swapValues(arr) {
+  return arr.reverse()
+}
+
+
+// ====== 8kyu Plural ======//
+
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+function plural(n) {
+  if (n === 1){
+    return false
+  }else {
+    return true
+  }
+}
