2eac023f5ed4c0f1f22f5889884236e33165b74b
dtothehill <danielhillegas@gmail.com>
2022-04-28 13:21:21 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-28/main.js
+++ 8kyu/CC 2022-04-28/main.js
@@ -121,3 +121,19 @@ function addExtra( listOfNumbers ){
 function past(h, m, s){
   return ((h*3600) + (m*60) + s)*1000
 }
+
+
+
+// ====== Beginner Series #1 School Paperwork =======//
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+function paperwork(n, m) {
+  if (n > 0 && m > 0){
+    return n * m
+  }else {
+    return 0
+  }
+}
