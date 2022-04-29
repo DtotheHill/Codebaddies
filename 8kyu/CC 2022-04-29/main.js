35d000135f524cd7165347ed2a30b97d9cceae38
dtothehill <danielhillegas@gmail.com>
2022-04-29 15:47:40 -0400

    added 8kyu solution

--- 8kyu/CC 2022-04-29/main.js
+++ 8kyu/CC 2022-04-29/main.js
@@ -118,3 +118,24 @@ class Person {
 function shortenToDate(longDate) {
   return longDate.split(',')[0]
 }
+
+// ======8kyu Basic: making six toast ======//
+
+// You forgot to count the number of toast you put into there, you don't know if you put exactly six pieces of toast into the toasters.
+
+// Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative.
+
+
+function sixToast(num) {
+  let needed = 6
+  
+  if(num < needed){
+    return needed - num
+  }else if (num > needed){
+    return num - needed
+  }else if (num === needed){
+   return 0
+  }else {
+    return num
+  }
+}
