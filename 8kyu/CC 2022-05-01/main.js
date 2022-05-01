e9c91919912e4a7eca5afc20591c37aeb4a47296
dtothehill <danielhillegas@gmail.com>
2022-05-01 16:30:26 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-01/main.js
+++ 8kyu/CC 2022-05-01/main.js
@@ -51,3 +51,13 @@ function multipleOfIndex(array) {
 function distanceBetweenPoints(a, b) {
   return Math.hypot(a.x - b.x, a.y - b.y)
 }
+
+// ====== 8kyu pick a set of first elements ======//
+
+// Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+// If n == 0 return an empty sequence []
+
+function first(arr, n=1) {
+  return arr.slice(0,n)
+}
