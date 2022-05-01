9c04bb9c40e7a5c5cd7697cc5182006648abe986
dtothehill <danielhillegas@gmail.com>
2022-05-01 16:28:33 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-01/main.js
+++ 8kyu/CC 2022-05-01/main.js
@@ -37,3 +37,17 @@ function multipleOfIndex(array) {
   return array.filter((num,i)=> num % i === 0)
 }
  
+
+// ======8kyu Geometry Basics: Distance between points in 2D ======//
+
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x and y attributes (X and Y in C#) attributes.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests round answers to 6 decimal places.
+
+function distanceBetweenPoints(a, b) {
+  return Math.hypot(a.x - b.x, a.y - b.y)
+}
