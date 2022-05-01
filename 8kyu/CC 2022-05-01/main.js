dd5c246de1052de9ea659f3bfc422cd5e16c5f57
dtothehill <danielhillegas@gmail.com>
2022-05-01 16:48:53 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-01/main.js
+++ 8kyu/CC 2022-05-01/main.js
@@ -104,3 +104,22 @@ function uefaEuro2016([t1,t2],[s1,s2]){
     return `At match ${t1} - ${t2}, ${t1} won!`
   }
 }
+
+// ====== 8kyu Sum of differences in array ======//
+
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// For example:
+
+// sumOfDifferences([2, 1, 10])
+// Returns 9
+
+// Descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+
+function sumOfDifferences(arr) {
+  return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0
+}
