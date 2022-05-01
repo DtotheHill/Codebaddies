776e4edae14ae24780eb59f0307c2ac73bd06648
dtothehill <danielhillegas@gmail.com>
2022-05-01 16:43:05 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-01/main.js
+++ 8kyu/CC 2022-05-01/main.js
@@ -76,3 +76,17 @@ const operations={
 function logicalCalc(array, op){
   return array.reduce(operations[op])
 }
+
+
+// ======= 8kyu Total pressure calculation ======//
+
+
+
+function solution(M1, M2, m1, m2, V, T) {
+  M1 = m1 * 0.001/M1;
+  M2 = m2 * 0.001/M2;
+  T = T + 273.15;
+var R = 0.082;
+
+return (((M1 + M2) * R * T) / V) * 1000;
+}
