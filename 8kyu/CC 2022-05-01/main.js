01e681c8a7a3e7008ea94e96a0985506a04ed0c1
dtothehill <danielhillegas@gmail.com>
2022-05-01 16:51:01 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-01/main.js
+++ 8kyu/CC 2022-05-01/main.js
@@ -123,3 +123,25 @@ function uefaEuro2016([t1,t2],[s1,s2]){
 function sumOfDifferences(arr) {
   return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0
 }
+
+// ====== 8kyu Classic Hello World ======//
+
+// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+// Note that for some languages, the function main is the entry point of the program.
+
+// Here's how it will be tested:
+
+//     Solution.main("parameter1","parameter2");
+// Hints:
+
+// Check your references
+// Think about the scope of your method
+// For prolog you can use write but there are better ways
+// If you still don't get it probably you can define main as an attribute of the Solution class that accepts a single argument, and that only prints "Hello World!" without any return.
+
+class Solution{
+  static main(){
+    console.log('Hello World!')
+  }
+}
