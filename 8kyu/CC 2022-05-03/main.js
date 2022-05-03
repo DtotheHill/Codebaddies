1991ca548a87c93374ee479b1c2db753c7adf54e
dtothehill <danielhillegas@gmail.com>
2022-05-03 17:03:10 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-03/main.js
+++ 8kyu/CC 2022-05-03/main.js
@@ -19,3 +19,15 @@ class Counter {
     this.value = 0;
   };
 }
+
+
+// ====== 8kyu Training JS #18: Methods of String object--concat() split() and its good friend join() ======//
+
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+
+function splitAndMerge(str, sp) {
+  return str.split(" ").map(word => word.split("").join(sp)).join(" ");
+}
