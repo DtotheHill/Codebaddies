c4da8e5e3c8a7d1c7903ebcb5fa66be224c8793f
dtothehill <danielhillegas@gmail.com>
2022-05-07 21:10:02 -0400

    added 8kyu solution

--- 8kyu/CC 2022-05-07/main.js
+++ 8kyu/CC 2022-05-07/main.js
@@ -15,3 +15,19 @@ var Person = function(){
   }
   return person;
 }
+
+
+// ======8kyu Swtich Case: Bug fixing #6 ======//
+
+// Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
+function evalObject(value){
+  switch(value.operation){
+    case'+': return value.a + value.b;
+    case'-': return value.a - value.b;
+    case'/': return value.a / value.b;
+    case'*': return value.a * value.b;
+    case'%': return value.a % value.b;
+    case'^': return Math.pow(value.a, value.b);
+  }
+}
