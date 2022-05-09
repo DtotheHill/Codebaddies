154f07718f93a4a8f1059bef136240107a398d99
dtothehill <danielhillegas@gmail.com>
2022-05-09 15:30:43 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -318,3 +318,22 @@ function howManySmaller(arr,n){
   return arr.filter( x => x.toFixed(2)<n).length;
   
 }
+
+// ====== 8kyu Training JS #16: Methods of String object--slice(), substring() and substr() ======//
+
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+// for example:
+
+// cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+// cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+// cutIt(["codewars","javascript","java"]) should return ["code","java","java"]
+
+const cutIt = arr => {
+  const minLength = Math.min(...arr.map(str => str.length))
+  return arr.map(str => str.slice(0, minLength))
+}
