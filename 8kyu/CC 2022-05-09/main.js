7fba3576562577b74e75f51e7926c6ea7ad17b33
dtothehill <danielhillegas@gmail.com>
2022-05-09 16:31:20 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -964,3 +964,17 @@ function sc(floor){
   
   return 'Aa~ '.repeat(floor-1) + 'Pa!' + (floor<=6 ? ' Aa!': '');
 }
+
+
+// ======8kyu Geometry Basics: Circle Circumference in 2D ======//
+
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+
+// Write a function calculating circumference of a Circle.
+
+// Tests round answers to 6 decimal places.
+
+const circleCircumference = circle => 2 * Math.PI * circle.radius;
+
