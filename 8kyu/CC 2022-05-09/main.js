4ed35539d4d8477aa3edd58d45ccb1155cccf3c8
dtothehill <danielhillegas@gmail.com>
2022-05-09 15:33:16 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -360,3 +360,31 @@ function firstToLast(str,c){
   return str.includes(c)?lenght:str.indexOf(c);
     
 }
+
+
+//======8kyu Training JS #21: Methods of String object--trim() and the string template ======//
+
+
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+// Note2: Using a string template can make your job easier.
+
+
+const fiveLine = s => {
+  const sWithoutWhitespace = s.trim()
+  let result = ''
+
+  for (let i = 1; i <= 5; i++) {
+    if (i === 5) {
+      result += `${sWithoutWhitespace.repeat(i)}`
+      break
+    }
+    result += `${sWithoutWhitespace.repeat(i)}\n`
+  }
+
+  return result
+}
