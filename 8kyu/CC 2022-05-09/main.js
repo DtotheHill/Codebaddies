61bd2abe16969196dafac559edb4358179fd2042
dtothehill <danielhillegas@gmail.com>
2022-05-09 16:14:22 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -803,3 +803,18 @@ class Ship {
      return this.draft - 1.5 * this.crew > 20;
    }
 }
+
+
+// ======8kyu Character Frequency ======//
+
+// Description
+// Welcome, Warrior! In this kata, you will get a message and you will need to get the frequency of each and every character!
+
+// Explanation
+// Your function will be called char_freq/charFreq/CharFreq and you will get passed a string, you will then return a dictionary (object in JavaScript) with as keys the characters, and as values how many times that character is in the string. You can assume you will be given valid input.
+
+function charFreq(message) {
+  var counts = {};
+  [...message].forEach( x => counts[x] ? counts[x]++ : counts[x] = 1);
+  return counts;
+}
