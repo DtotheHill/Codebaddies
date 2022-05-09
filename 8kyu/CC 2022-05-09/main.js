89c8f31fa6ed7405d64a224e1e13f8afd326b11b
dtothehill <danielhillegas@gmail.com>
2022-05-09 15:22:17 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -159,4 +159,36 @@ const bloodAlcoholContent = ({ ounces, abv }, weight, sex, time) =>
       ounces * abv * 5.14 / weight * (sex === 'male' ? 0.73 : 0.66) -
       0.015 * time
     ).toFixed(4)
-  )
+)
+
+
+
+
+// ======8kyu Training JS #9: loop statement --while and do..while =======//
+
+// Coding in function padIt, function accept 2 parameters:
+
+// str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+// n, it's a number, how many times to pad the string.
+// Behaviour
+// You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+// Finally, return the padded string.
+
+
+const padIt = (str, n) => {
+  let result = str
+  let i = 0
+
+  while (i < n) {
+    if (i % 2 === 0) {
+      result = `*${result}`
+    } else {
+      result = `${result}*`
+    }
+
+    i += 1
+  }
+
+  return result
+}
