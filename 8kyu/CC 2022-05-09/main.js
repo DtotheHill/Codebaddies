5c8e6d6233b9982605cb30691d096cb4d6e40bc8
dtothehill <danielhillegas@gmail.com>
2022-05-09 16:09:38 -0400

    added solution

--- 8kyu/CC 2022-05-09/main.js
+++ 8kyu/CC 2022-05-09/main.js
@@ -716,3 +716,33 @@ function startingMark(bodyHeight){
 function duckDuckGoose(players, goose) {
   return players[(goose-1)%players.length].name
 }
+
+// ======8kyu Simple Fun #261: Whose Move =======//
+
+// Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
+
+// Input/Output
+// [input] string lastPlayer/$last_player
+
+// "black" or "white" - whose move it was during the previous round.
+
+// [input] boolean win/$win
+
+// true if the player who made a move during the previous round won, false otherwise.
+
+// [output] a string
+
+// Return "white" if white is to move on the next round, and "black" otherwise.
+
+function whoseMove(lastPlayer, win) {
+
+  if(lastPlayer === 'black' && win === true) {
+    return 'black';
+  } else if(lastPlayer === 'black' && win === false) {
+    return 'white';
+  } else if(lastPlayer === 'white' && win === true) {
+    return 'white';
+  } else if(lastPlayer === 'white' && win === false) {
+    return 'black';
+  }
+}
